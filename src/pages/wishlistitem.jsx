e1b3bf35169cbea9f1949497a2f0ac019f84f20a
sowmiya Ramraj <109214296+sowmiyaramraj@users.noreply.github.com>
2023-10-03 14:16:52 +0530

    Update wishlistitem.jsx

--- src/pages/wishlistitem.jsx
+++ src/pages/wishlistitem.jsx
@@ -1,16 +1,31 @@
 import React,{useContext} from "react";
 import { product } from "../components/products";
 import { ShopContext } from "../context/shop-context";
-
+import {Card,Box,Grid,Typography, CardMedia,CardContent, CardHeader  } from "@mui/material";
 export const Wishlistitem=(props)=>{
-    const {cartItem,addtowistlist,removefromwistlist}=useContext(ShopContext);
-    const {productName,productId,productImage,productPrice}=props.data;
-
+    const {cartItem,addtocart,removefromcart,addtowishlist,updatecartitemcount,removefromwistlist}=useContext(ShopContext);
+    const {productName,productId,productImage,productDescription,productPrice}=props.data;
+    const amount=cartItem[productId];
+    
     return(
         <div>
-           <p>{productName}</p>
-            <p>{productId}</p>
-            <p>{productPrice}</p>
+            <br/>
+            <Card style={{width:"300px",height:"300px"}}>
+                <CardMedia component="img"
+                            height="180"
+                            width="200"
+                            image={productImage}
+                            alt="Product">
+
+                </CardMedia>
+                <CardContent>
+                    {productName}<br/>
+                    {productDescription}<br/>
+                    ₹{productPrice}
+                </CardContent>
+               
+            </Card>
         </div>
+       
     );
 }
