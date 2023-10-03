6df4cc2b3d8fd1a2c0da30d6fede129d4a41c6ef
sowmiya Ramraj <109214296+sowmiyaramraj@users.noreply.github.com>
2023-10-03 14:18:20 +0530

    Update cartitem.jsx

--- src/pages/cartitem.jsx
+++ src/pages/cartitem.jsx
@@ -1,19 +1,31 @@
 import React,{ useContext } from "react";
 import { ShopContext } from "../context/shop-context";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 export const CartItem=(props)=>{
-    const {productName,productId,productImage,productPrice}=props.data;
+    const {productName,productDescription,productId,productImage,productPrice}=props.data;
     const {addtocart,cartItem,removefromcart,updatecartitemcount}=useContext(ShopContext);
     return(
         <div>
-            <p>{productName}</p>
-            <p>{productId}</p>
-            <p>{productPrice}</p>
-            <button onClick={()=>addtocart(productId)}>+</button>
+            <br/>
+            <Card style={{width:"300px",height:"300px"}}>
+                <CardMedia component="img"
+                            height="180"
+                            width="200"
+                            image={productImage}
+                            alt="Product">
+
+                </CardMedia>
+                <CardContent>
+                    {productName}<br/>
+                    {productDescription}<br/>
+                    ₹{productPrice}
+                </CardContent>
+                <button onClick={()=>addtocart(productId)}>+</button>
             <input value={cartItem[productId]} onChange={(e)=>updatecartitemcount(Number(e.target.value) ,productId)}/>
             <button onClick={()=>removefromcart(productId)}>-</button>
 
-
+            </Card>
         </div>
     );
 }
